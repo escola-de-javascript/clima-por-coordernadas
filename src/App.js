025584c45fae460ea9320f0e6511b8f191df6c97
leonardo <leonardoscorzas88@gmail.com>
2019-08-13 11:32:51 -0300

    projeto completo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
+
+if (location == false) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      Você precisa habilitar a localização no browser o/
+    </Fragment>
+  )
+} else if (weather == false) {
+  return (
+    <Fragment>
+      Carregando o clima...
+    </Fragment>
+  )
+} else {
+  return (
+    <Fragment>
+      <h3>Clima nas suas Coordenadas ({weather['weather'][0]['description']})</h3>
+      <hr/>
+      <ul>
+        <li>Temperatura atual: {weather['main']['temp']}°</li>
+        <li>Temperatura máxima: {weather['main']['temp_max']}°</li>
+        <li>Temperatura minima: {weather['main']['temp_min']}°</li>
+        <li>Pressão: {weather['main']['pressure']} hpa</li>
+        <li>Umidade: {weather['main']['humidity']}%</li>
+      </ul>
+    </Fragment>
   );
+}
 }
 
 export default App;
